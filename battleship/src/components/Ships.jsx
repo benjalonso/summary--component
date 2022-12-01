6d7bd6b31ff8fb16d362026fcfcdb996ff75b586
Benjamín Larenas <alonsobenjaminlb@gmail.com>
2022-12-01 12:07:05 -0400

    avance ui

--- battleship/src/components/Ships.jsx
+++ battleship/src/components/Ships.jsx
@@ -4,33 +4,33 @@ const Ships = () => {
 
 
   return (
-    <div className="bg-slate-500 h-32 w-52 mt-10">
-      <div className="m-1 flex flex-row" draggable={true}>
-        <div className=" w-5 h-5 bg-green-200" id="destroyer-0"></div>
-        <div className=" w-5 h-5 bg-green-200" id="destroyer-1"></div>
+    <div className="bg-slate-500 h-32 w-52 mt-5">
+      <div className="m-1 flex flex-row w-min" draggable={true}>
+        <div className=" w-6 h-6 bg-green-200" id="destroyer-0"></div>
+        <div className=" w-6 h-6 bg-green-200" id="destroyer-1"></div>
       </div>
-      <div className="m-1 flex flex-row" draggable={true}>
-        <div className=" w-5 h-5 bg-green-200" id="submarine-0"></div>
-        <div className=" w-5 h-5 bg-green-200" id="submarine-1"></div>
-        <div className=" w-5 h-5 bg-green-200" id="submarine-2"></div>
+      <div className="m-1 flex flex-row w-min" draggable={true}>
+        <div className=" w-6 h-6 bg-green-200" id="submarine-0"></div>
+        <div className=" w-6 h-6 bg-green-200" id="submarine-1"></div>
+        <div className=" w-6 h-6 bg-green-200" id="submarine-2"></div>
       </div>
-      <div className="m-1 flex flex-row" draggable={true}>
-        <div className=" w-5 h-5 bg-green-200" id="cruiser-0"></div>
-        <div className=" w-5 h-5 bg-green-200" id="cruiser-1"></div>
-        <div className=" w-5 h-5 bg-green-200" id="cruiser-2"></div>
+      <div className="m-1 flex flex-row w-min" draggable={true}>
+        <div className=" w-6 h-6 bg-green-200" id="cruiser-0"></div>
+        <div className=" w-6 h-6 bg-green-200" id="cruiser-1"></div>
+        <div className=" w-6 h-6 bg-green-200" id="cruiser-2"></div>
       </div>
-      <div className="m-1 flex flex-row" draggable={true}>
-        <div className=" w-5 h-5 bg-green-200" id="battleship-0"></div>
-        <div className=" w-5 h-5 bg-green-200" id="battleship-1"></div>
-        <div className=" w-5 h-5 bg-green-200" id="battleship-2"></div>
-        <div className=" w-5 h-5 bg-green-200" id="battleship-3"></div>
+      <div className="m-1 flex flex-row w-min" draggable={true}>
+        <div className=" w-6 h-6 bg-green-200" id="battleship-0"></div>
+        <div className=" w-6 h-6 bg-green-200" id="battleship-1"></div>
+        <div className=" w-6 h-6 bg-green-200" id="battleship-2"></div>
+        <div className=" w-6 h-6 bg-green-200" id="battleship-3"></div>
       </div>
-      <div className="m-1 flex flex-row" draggable={true}>
-        <div className=" w-5 h-5 bg-green-200" id="carrier-0"></div>
-        <div className=" w-5 h-5 bg-green-200" id="carrier-1"></div>
-        <div className=" w-5 h-5 bg-green-200" id="carrier-2"></div>
-        <div className=" w-5 h-5 bg-green-200" id="carrier-3"></div>
-        <div className=" w-5 h-5 bg-green-200" id="carrier-4"></div>
+      <div className="m-1 flex flex-row w-min" draggable={true}>
+        <div className=" w-6 h-6 bg-green-200" id="carrier-0"></div>
+        <div className=" w-6 h-6 bg-green-200" id="carrier-1"></div>
+        <div className=" w-6 h-6 bg-green-200" id="carrier-2"></div>
+        <div className=" w-6 h-6 bg-green-200" id="carrier-3"></div>
+        <div className=" w-6 h-6 bg-green-200" id="carrier-4"></div>
       </div>
     </div>
   );
--- battleship/src/components/Table.jsx
+++ battleship/src/components/Table.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import Square from "./Square";
 import Ships from "./Ships";
+
 export const Table = () => {
-  const table = new Array(100).fill(null);
+
+const table = new Array(100).fill(null);
+let hostTable = table.fill(<Square/>)
+let enemysTable = table.fill(<Square/>)
+let directions = ['x', 'y']
+function generate(direction, ship, squares) {
+  let randomDirection = dir[Math.floor(Math.random() * direction.length)];
+  let current = ship.directions[randomDirection];
+}
 
   return (
     <>
-      <div>
-        <button id="start">Start Game</button>
-        <button id="randomize">Randomize Ships</button>
-        <button id="reset">Reset Game</button>
-        <h3>Your turn</h3>
-        <h4>
-          <span id="host-score">0</span> - <span id="guest-score">0</span>
+      <div className="bg-slate-300 m-2 rounded">
+        <div className="grid grid-cols-3 place-content-center">
+          <button className="border border-cyan-800 m-2" id="start">
+            Start Game
+          </button>
+          <button className="border border-cyan-800 m-2" id="randomize">
+            Randomize Ships
+          </button>
+          <button className="border border-cyan-800 m-2" id="reset">
+            Reset Game
+          </button>
+        </div>
+        <h3 className="text-center text-xl">Your turn</h3>
+        <h4 className="text-center ">
+          <span id="host-score">0</span > - <span id="guest-score">0</span>
         </h4>
       </div>
       <div className="grid grid-cols-2 gap-20">
-        <div className="grid grid-cols-10">{table.fill(<Square />)}</div>
-        <div className="grid grid-cols-10 ">{table.fill(<Square />)}</div>
+        <div className="grid grid-cols-10">{hostTable}</div>
+        <div className="grid grid-cols-10 ">{enemysTable}</div>
       </div>
       <Ships />
     </>
